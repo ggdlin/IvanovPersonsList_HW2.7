a20dc920481a85b008dcaf4ca7e1ebf99d36102d
Sergey Ivanov <ggdlin@gmail.com>
2020-11-04 01:01:34 +0300

    Initial Commit

---
+++ IvanovPersonsList_HW2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IvanovPersonsList_HW2.7
+//
+//  Created by Admin on 04.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
